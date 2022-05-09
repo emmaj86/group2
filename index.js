3f985ec4b639156b1300e09538f7b98edc15c64c
Christina Mifsud <christina.mifsud@gmail.com>
2022-05-09 17:12:34 +0100

    updated footer & js experimentation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
+    setTimeout(function () {
+        document.getElementById('heading').innerHTML += 'CLICK ON A PLANET';
+    }, 2000);
 
 
 
