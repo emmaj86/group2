b66731e798dc6774ce4f7c91d62105489f38b3f3
Christina Mifsud <christina.mifsud@gmail.com>
2022-05-10 17:59:20 +0100

    relink & rename js

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    setTimeout(function () {
+        document.getElementById('heading').innerHTML += 'CLICK ON A PLANET';
+    }, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+})
